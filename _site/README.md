adb8a2cc23a61863660344183a7b58593ae53cb7
CharisseHao <charissehao@gmail.com>
2025-03-12 18:41:01 -0700

    Base code

---
+++ _site/README.md
@@ -0,0 +1,16 @@
+# LLM Biases in a Housing Context
+
+This repository hosts the website for the "LLM_bias_in_housing" project, which is focused on investigating and auditing potential biases in LLM (Large Language Models) responses in housing contexts. The website serves as a public interface to share findings, methodologies, and data visualizations derived from the original research conducted in the main repository.
+
+## Quick Links
+- **Main Project Repository:** [LLM_bias_in_housing](https://github.com/CharisseHao/LLM_bias_in_housing)
+
+- **Website Link**: [LLM bias in housing website](https://josephguzman03.github.io/LLM_Housing.github.io/) 
+
+## Getting Started
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/your-username/capstone-website.git
+
+   
---
+++ _site/static/js/index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scrolling for navigation links
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = link.getAttribute("href");
+            const targetElement = (targetId === "#") ? document.body : document.querySelector(targetId);
+  
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+  
+  });
+  
+  $(document).ready(function(){
+      $("#intro .container").load("sections/intro.html", function() { setupFadeInObserver(); });
+      $("#methods .container").load("sections/methods.html", function() { setupFadeInObserver(); });
+      $("#prompts .container").load("sections/prompts.html", function() { setupFadeInObserver(); });
+      $("#apps .container").load("sections/apps.html", function() { setupFadeInObserver(); });
+      $("#team .container").load("sections/team.html", function() { setupFadeInObserver(); });
+      $("#results .container").load("sections/results.html", function() { setupFadeInObserver(); });
+      $("#conclusion .container").load("sections/conclusion.html", function() { setupFadeInObserver(); });
+      $("#differences .container").load("sections/differences.html", function() { setupFadeInObserver(); });
+  
+  });
